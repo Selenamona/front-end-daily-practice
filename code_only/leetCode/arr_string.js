d3bd877d67c7f3d1eb5d8eca6325902767e081d1
selenamona <1591541371@qq.com>
2021-07-16 11:53:23 +0800

    str_num

--- code_only/leetCode/arr_string.js
+++ code_only/leetCode/arr_string.js
@@ -84,21 +84,26 @@ var searchInsert = function (nums, target) {
  * @return {number[][]}
  */
 var merge = function (intervals) {
-  intervals.sort((a, b) => {
-    return a[0] - b[0]
-  })
-  intervals.reduce((a, b, c, d) => {
-    console.log(a, b);
-  })
-  return 1
+  intervals.sort((a, b) => a[0] - b[0]) //依次排序
+  return intervals.reduce((prev, cur, index) => {
+    let peek = prev[prev.length - 1] //获取到prev中最后一项
+    if (peek && peek[1] >= cur[0]) { //当前项和peek存在交集
+      let left = peek[0]
+      let right = peek[1] > cur[1] ? peek[1] : cur[1]
+      prev[prev.length - 1] = [left, right]
+    } else {
+      prev.push(cur)
+    }
+    return prev
+  }, [])
 };
 
-console.log(merge([
-  [2, 6],
-  [1, 3],
-  [8, 10],
-  [15, 18]
-]))
+// console.log(merge([
+//   [1, 3],
+//   [2, 6],
+//   [8, 10],
+//   [15, 18]
+// ]))
 
 
 // sort  test
@@ -115,3 +120,86 @@ console.log(merge([
 
 // console.log(ls, 1);
 // console.log(bv, 2);
+
+/**
+ * 题4 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  // 1、未原地旋转
+  // matrix.reverse()
+  // let res = []
+  // matrix.forEach((arr, index) => {
+  //   arr.forEach((num, k) => {
+  //     if (!res[k]) {
+  //       res[k] = []
+  //     }
+  //     res[k].push(num) e
+  //   })
+  // })
+  // res.forEach((item, n) => {
+  //   item.forEach((ele, k) => {
+  //     matrix[n][k] = ele
+  //   })
+  // })
+  // return res
+
+  // 2、未原地旋转
+  // matrix.reverse()
+  // matrix = matrix.reduce((pre, cur, index, arr) => {
+  //   cur.map((item, k) => {
+  //     if (!pre[k]) pre[k] = []
+  //     pre[k].push(item)
+  //   })
+  //   return pre
+  // }, [])
+
+  // 3、原地 旋转图像，这意味着你需要直接修改输入的二维矩阵
+  const n = matrix.length;
+  for (let i = 0; i < Math.floor(n / 2); ++i) {
+    for (let j = 0; j < Math.floor((n + 1) / 2); ++j) {
+      const temp = matrix[i][j];
+      console.log(i, j, temp);
+      console.log("temp", temp);
+
+      matrix[i][j] = matrix[n - j - 1][i];
+      console.log(" matrix[n - j - 1][i]", matrix[n - j - 1][i]);
+
+      matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+      console.log("matrix[n - i - 1][n - j - 1]", matrix[n - i - 1][n - j - 1]);
+
+      matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+      console.log("matrix[j][n - i - 1]", matrix[j][n - i - 1]);
+
+      matrix[j][n - i - 1] = temp;
+    }
+  }
+
+};
+
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ]
+
+const matrix = [
+  [5, 1, 9, 11],
+  [2, 4, 8, 10],
+  [13, 3, 6, 7],
+  [15, 14, 12, 16]
+]
+// 5 15 16 11
+// 1 13 12 10
+// 2 14 7 9
+// 4 3 6 8
+
+// 规律：matrixnew[col][n−row−1]=matrix[row][col]
+
+// 输出：
+// [15, 13, 2, 5],
+// [14, 3, 4, 1],
+// [12, 6, 8, 9],
+// [16, 7, 10, 11]
+console.log(rotate(matrix))
